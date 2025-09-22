4b7722f260e8456a7c7cf9b1ba68917e9469b5ab
venkatesh <venkatesh@group-ae.ea>
2025-09-22 12:05:44 +0530

    app

--- app.py
+++ app.py
@@ -10,10 +10,9 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 # Load configuration from JSON file
 config = {}
 config_paths = [
-    'sap_login/credential.json',  # Primary credential path
-    'config.json',                # Fallback to old path
+    os.path.join("C:", "tmp", "sap_login", "credential.json")
 ]
-
+print(config_paths)
 config_loaded = False
 for config_path in config_paths:
     try:
